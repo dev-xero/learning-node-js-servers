238df499585a3094f0d89c0dc7fed836c2d96fff
CodeNinja-tech <elo48858@gmail.com>
2022-09-29 22:15:37 +0100

    switched to ejs

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require('express');
+
 // CREATE AN EXPRESS APP
 const app = express();
+
+// SET EJS AS VIEW ENGINE
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
   console.log('listening for requests on port 3000');
 });
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
-  console.log('served file to the client');
+  res.render('index')
 });
 
 app.get('/about', (req, res) => {
   res.sendFile('./views/about.html', { root: __dirname });
-  console.log('served file to the client');
-});
-
-// REDIRECTS
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-  console.log('redirected user to /about');
 });
 
 // 404 PAGES
