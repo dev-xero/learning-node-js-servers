2ada77a43188d55fd16132ce90ba949692545729
CodeNinja-tech <elo48858@gmail.com>
2022-09-29 23:06:03 +0100

    learned Server Side Rendering SSR

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const app = express();
 // SET EJS AS VIEW ENGINE
 app.set('view engine', 'ejs');
 
+const blogs = [
+  {
+    title: 'Chainsaw Man',
+    snippet: 'lorem ipsum dolor sit amet',
+  },
+  {
+    title: 'Monkey D Luffy',
+    snippet: 'lorem ipsum dolor sit amet',
+  },
+  {
+    title: 'Mob Psycho III',
+    snippet: 'lorem ipsum dolor sit amet',
+  },
+];
+
 app.listen(3000, () => {
   console.log(
     'listening for requests on port 3000, site: http://localhost:3000'
@@ -13,18 +28,18 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', { title: 'Create Blog' });
 });
 
 // 404 PAGES
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { title: '404' });
 });
