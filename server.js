2ddfd6c854240d3a0c385f94b202ef6b19b20405
CodeNinja-tech <elo48858@gmail.com>
2022-09-29 22:37:59 +0100

    added other views to the app

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
-  console.log('listening for requests on port 3000');
+  console.log(
+    'listening for requests on port 3000, site: http://localhost:3000'
+  );
 });
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
 });
 
 // 404 PAGES
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });
