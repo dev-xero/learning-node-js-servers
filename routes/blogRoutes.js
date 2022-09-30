2981c55f3584b3bcde31968185c0e5f273887522
CodeNinja-tech <elo48858@gmail.com>
2022-09-30 23:28:55 +0100

    updated MVC and Router

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const blogController = require('../controllers/blogController');
 const router = express.Router();
 
-router.get('/', (req, res) => blogController.blog_index(req, res));
-router.get('/create', (req, res) => blogController.blog_create_get(req, res));
-router.get('/:id', (req, res) => blogController.blog_details(req, res));
-router.post('/', (req, res) => blogController.blog_create_post(req, res));
-router.delete('/:id', (req, res) => blogController.blog_create_get(req, res));
+router.get('/', blogController.blog_index);
+router.get('/create', blogController.blog_create_get);
+router.get('/:id', blogController.blog_details);
+router.post('/', blogController.blog_create_post);
+router.delete('/:id', blogController.blog_create_get);
 
 module.exports = router;
